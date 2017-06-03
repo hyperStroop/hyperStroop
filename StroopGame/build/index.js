e985f6849c5cb73d99518f49ae8bd23d0bcf117c
BossJ <julian.boss@hotmail.com>
2017-06-02 18:45:57 -0700

    added start command to controller/removed bootstrap

--- StroopGame/build/index.js
+++ StroopGame/build/index.js
@@ -1,8 +1,9 @@
+// import 'bootstrap'; //bootstrap.js for button toggling
 "use strict";
-require("bootstrap"); //bootstrap.js for button toggling
 var model_1 = require("./model");
 var game_view_1 = require("./game-view");
 var controller_1 = require("./controller");
 var model = new model_1.Model();
 var view = new game_view_1.View(model);
 var controller = new controller_1.Controller(model, view);
+controller.start();
