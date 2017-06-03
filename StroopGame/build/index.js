67b9654eacacf9b6db135e3035b6bf94e3815157
BossJ <julian.boss@hotmail.com>
2017-06-02 18:31:41 -0700

    changed back

--- StroopGame/build/index.js
+++ StroopGame/build/index.js
@@ -1,5 +1,5 @@
 "use strict";
-global.jQuery = global.$ = require('jquery'); 
+global.jQuery = require('jquery'); 
 require("bootstrap"); //bootstrap.js for button toggling
 var model_1 = require("./model");
 var game_view_1 = require("./game-view");
