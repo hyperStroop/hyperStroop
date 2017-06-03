c39f44db836bd2b665f5819b99e2445f7f115f4d
BossJ <julian.boss@hotmail.com>
2017-06-02 18:33:27 -0700

    removed require line

--- StroopGame/build/index.js
+++ StroopGame/build/index.js
@@ -1,5 +1,4 @@
 "use strict";
-global.jQuery = require('jquery'); 
 require("bootstrap"); //bootstrap.js for button toggling
 var model_1 = require("./model");
 var game_view_1 = require("./game-view");
